97490d59b00082e294a4b51631bd6e49c9ec9ae5
昌维 <867597730@qq.com>
2020-02-10 04:26:42 +0800

    add leftsidebar skeleton

---
+++ pgui/src/pages/design/LeftSidebar/index.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+
+import './index.sass'
+import Collapse from "components/Collapse";
+
+export default
+class LeftSidebar extends Component {
+    render() {
+        return (
+            <div className="LeftSidebar">
+                LeftSidebar
+                <Collapse title={'Canvas Manager'}>Collapse</Collapse>
+                <Collapse title={'Control Library'}>Collapse</Collapse>
+                <Collapse title={'Element Manager'}>Collapse</Collapse>
+            </div>
+        )
+    }
+}
