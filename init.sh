183109c6f81c5047db6bb710d8d78bb8065efc78
昌维 <867597730@qq.com>
2020-02-10 03:01:23 +0800

    add npm package.json and init.sh and start.sh

---
+++ init.sh
@@ -0,0 +1,7 @@
+# init pgui front-end react project
+# code by cw1997 at 2020-02-10 02:58:01
+npx create-react-app pgui
+cd pgui
+npm install --save react-router-dom redux react-redux office-ui-fabric-react
+npm install --save-dev redux-devtools node-sass
+npm start
---
+++ start.sh
@@ -0,0 +1,3 @@
+# start pgui front-end react project
+# code by cw1997 at 2020-02-10 02:58:01
+npm start
