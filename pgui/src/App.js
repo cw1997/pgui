be43dea2d7c8b7cca4b833aa3ce97e5e625b6d09
昌维 <867597730@qq.com>
2020-02-10 04:41:29 +0800

    use grid layout

--- pgui/src/App.js
+++ pgui/src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import './App.sass';
-import LeftSidebar from "./pages/design/LeftSidebar";
+import LeftSidebar from "pages/design/LeftSidebar";
+import RightSidebar from "pages/design/RightSidebar";
 
 function App() {
-  return (
-    <div className="App">
-        App test
-        <LeftSidebar/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="design-area">
+                <div className="left-sidebar">
+                    <LeftSidebar/>
+                </div>
+                <div className="canvas-area">
+                    Canvas
+                </div>
+                <div className="right-sidebar">
+                    <RightSidebar/>
+                </div>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
