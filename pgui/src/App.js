a0f1caa90c97ca4fe3d255f4ce67a6c1081b719b
昌维 <867597730@qq.com>
2020-02-10 15:35:09 +0800

    add redux provider

--- pgui/src/App.js
+++ pgui/src/App.js
@@ -3,23 +3,28 @@ import './App.sass';
 import LeftSidebar from "pages/design/LeftSidebar";
 import Canvas from "pages/design/Canvas";
 import RightSidebar from "pages/design/RightSidebar";
+import {createStore} from "redux";
+import pGui from "redux/reduces";
+import {Provider} from "react-redux";
 
 function App() {
+    let store = createStore(pGui);
     return (
-        <div className="App">
-            <div className="design-area">
-                <div className="left-sidebar">
-                    <LeftSidebar/>
-                </div>
-                <div className="canvas-area">
-                    <Canvas/>
-                </div>
-                <div className="right-sidebar">
-                    <RightSidebar/>
+        <Provider store={store}>
+            <div className="App">
+                <div className="design-area">
+                    <div className="left-sidebar">
+                        <LeftSidebar/>
+                    </div>
+                    <div className="canvas-area">
+                        <Canvas/>
+                    </div>
+                    <div className="right-sidebar">
+                        <RightSidebar/>
+                    </div>
                 </div>
             </div>
-
-        </div>
+        </Provider>
     );
 }
 
