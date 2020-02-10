526344d8a970eb9eeb23a40170c39a8457e8582c
昌维 <867597730@qq.com>
2020-02-11 03:52:28 +0800

    inject Appearance and Behavior

--- pgui/src/pages/design/RightSidebar/index.js
+++ pgui/src/pages/design/RightSidebar/index.js
@@ -1,15 +1,22 @@
 import React, {Component} from "react";
+import { SpinButton } from 'office-ui-fabric-react/lib/SpinButton';
 
 import './index.sass'
 import Collapse from "components/Collapse";
+import Appearance from "pages/design/RightSidebar/Appearance";
+import Behavior from "pages/design/RightSidebar/Behavior";
 
 export default
 class RightSidebar extends Component {
     render() {
         return (
             <div className="RightSidebar">
-                <Collapse title={'Appearance'}>Collapse</Collapse>
-                <Collapse title={'Behavior'}>Collapse</Collapse>
+                <Collapse title={'Appearance'}>
+                    <Appearance/>
+                </Collapse>
+                <Collapse title={'Behavior'}>
+                    <Behavior/>
+                </Collapse>
             </div>
         )
     }
