6a4f12fcec43cbd44a705ae9efe200cd59aedb43
昌维 <867597730@qq.com>
2020-02-10 15:54:53 +0800

    add redux-devtools

--- pgui/src/App.js
+++ pgui/src/App.js
@@ -3,12 +3,16 @@ import './App.sass';
 import LeftSidebar from "pages/design/LeftSidebar";
 import Canvas from "pages/design/Canvas";
 import RightSidebar from "pages/design/RightSidebar";
-import {createStore} from "redux";
+import {compose, createStore} from "redux";
 import pGui from "redux/reduces";
 import {Provider} from "react-redux";
 
+// add redux-devtools by cw1997 2020-02-10 15:54:28
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 function App() {
-    let store = createStore(pGui);
+    // let store = createStore(pGui);
+    const store = createStore(pGui, /* preloadedState, */ composeEnhancers());
     return (
         <Provider store={store}>
             <div className="App">
