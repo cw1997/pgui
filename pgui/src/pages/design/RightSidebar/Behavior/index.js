cd0440c796b1c9a83752e13790ac59527dc1fd26
昌维 <867597730@qq.com>
2020-02-11 03:51:49 +0800

    add Behavior

---
+++ pgui/src/pages/design/RightSidebar/Behavior/index.js
@@ -0,0 +1,30 @@
+import React, {Component} from "react";
+import { SpinButton } from 'office-ui-fabric-react/lib/SpinButton';
+
+import './index.sass'
+
+export default
+function Behavior(props) {
+    return (
+        <div className="Behavior ms-Fabric">
+            <ConfigItem label={'Behavior'}>
+                <select>
+                    <option value={'exec'}>Execute Code</option>
+                </select>
+            </ConfigItem>
+        </div>
+    )
+}
+
+function ConfigItem(props) {
+    return (
+        <div className="ConfigItem">
+            <div className="config-item-label">
+                {props.label}
+            </div>
+            <div className="config-item-input">
+                {props.children}
+            </div>
+        </div>
+    )
+}
